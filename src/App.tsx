0c2ff3c68ee71eb08476790b2fbfd4fdaed135ba
Cassio-Vileno <cassiovileno@gmail.com>
2023-08-15 11:02:43 -0300

    vercel analitcs

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.scss";
 import Home from "./screens/Home";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <main className="container_main">
+      <Analytics />
       <Home />
     </main>
   );
